3da2cc9c20443d3fcdd28ae8a64c9c4129b0e7f8
Coding Shenanigans <94819102+coding-shenanigans@users.noreply.github.com>
2026-03-08 00:32:23 -0800

    Add sign in form and zustand for state management (#4)

---
+++ src/zustand/store.ts
@@ -0,0 +1,13 @@
+import { create } from "zustand";
+import {
+  createUserSessionSlice,
+  type UserSessionSlice,
+} from "./user-session-slice";
+
+// Combine other slices here.
+// Example: AppStore = firstSlice & secondSlice & thirdSlice
+export type AppStore = UserSessionSlice;
+
+export const useAppStore = create<AppStore>()((...a) => ({
+  ...createUserSessionSlice(...a),
+}));
