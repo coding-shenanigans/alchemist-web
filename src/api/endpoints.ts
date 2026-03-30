4dfa19c66b3a69d760d1c81c4588ac2c1745b256
coding-shenanigans <coding.shenanigans@gmail.com>
2026-03-29 22:45:21 -0700

    Implement sign up functionality

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -4,6 +4,8 @@ import type {
   ErrorResponse,
   SignInRequest,
   SignInResponse,
+  SignUpRequest,
+  SignUpResponse,
 } from "../types";
 import client from "./client";
 import { useAppStore } from "../zustand/store";
@@ -20,6 +22,31 @@ export const signIn = async (
 ): Promise<ApiResponse<SignInResponse>> => {
   try {
     const response = await client.post<SignInResponse>("/auth/signin", request);
+    setUserSession(response.data.userSession);
+
+    return { status: response.status, data: response.data };
+  } catch (error) {
+    if (axios.isAxiosError<ErrorResponse>(error) && error.response) {
+      return {
+        status: error.response.data.error.code,
+        error: new Error(error.response.data.error.message),
+      };
+    }
+
+    return { status: 500, error: new Error("An unknown error occurred") };
+  }
+};
+
+/**
+ * Starts a user session.
+ * @param request The request object.
+ * @returns An API response object.
+ */
+export const signUp = async (
+  request: SignUpRequest,
+): Promise<ApiResponse<SignUpResponse>> => {
+  try {
+    const response = await client.post<SignUpResponse>("/auth/signup", request);
     setUserSession(response.data.userSession);
 
     return { status: response.status, data: response.data };
