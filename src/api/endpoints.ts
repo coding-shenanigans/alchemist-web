1f3e0e40145927a2e2b3737345117af3a07ad6f2
Coding Shenanigans <94819102+coding-shenanigans@users.noreply.github.com>
2026-04-25 22:44:28 -0700

    Implement refresh functionality. (#18)
    HTTP requests that fail with a 401 (Unauthenticated) can be retried after the user session is refreshed.

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -1,15 +1,105 @@
 import type {
   ApiResponse,
+  ErrorResponse,
+  RefreshResponse,
   SignInRequest,
   SignInResponse,
   SignUpRequest,
   SignUpResponse,
 } from "../types";
 import { useAppStore } from "../zustand/store";
-import { sendRequest } from "./client";
 
 // TODO: Fetch backend url from env or constants file.
 const baseUrl = "http://localhost:9000";
+
+let refreshPromise: Promise<ApiResponse<RefreshResponse>> | null = null;
+
+/**
+ * Sends a fetch request.
+ * @param url The URL for the request.
+ * @param options The request options.
+ * @param retry Whether unauthenticated errors should be retried.
+ * @returns An ApiResponse object.
+ */
+const sendRequest = async <T>(
+  url: string,
+  options: RequestInit,
+  retry: boolean,
+): Promise<ApiResponse<T>> => {
+  try {
+    const response = await fetch(url, options);
+
+    // Handle success responses without a body (204 No Content).
+    const data = response.status === 204 ? {} : await response.json();
+
+    if (response.ok) {
+      return { status: response.status, data };
+    }
+
+    if (response.status === 401 && retry) {
+      return await refreshAndRetry<T>(url, options);
+    }
+
+    const errorResponse = data as ErrorResponse;
+    return {
+      status: response.status,
+      error: new Error(errorResponse.error.message),
+    };
+  } catch (error) {
+    console.error(`[API Error] ${options.method} ${url}`, error);
+
+    return { status: 500, error: new Error("An unknown error occurred.") };
+  }
+};
+
+/**
+ * Retries a request after refreshing the user session.
+ * @param url The URL for the request.
+ * @param options The request options.
+ * @returns An ApiResponse object.
+ */
+const refreshAndRetry = async <T>(
+  url: string,
+  options: RequestInit,
+): Promise<ApiResponse<T>> => {
+  const { error } = await getRefreshPromise();
+
+  if (error) {
+    signOut();
+    return { status: 401, error: new Error("The user session expired.") };
+  }
+
+  const { userSession } = useAppStore.getState();
+  const newOptions = {
+    ...options,
+    headers: {
+      ...options.headers,
+      Authorization: `Bearer ${userSession?.accessToken}`,
+    },
+  };
+
+  return await sendRequest<T>(url, newOptions, false);
+};
+
+/**
+ * Gets a promise singleton containing the response from the refresh request.
+ *
+ * This ensures that there's only one active refresh request at a time.
+ * @returns An ApiResponse object.
+ */
+const getRefreshPromise = async (): Promise<ApiResponse<RefreshResponse>> => {
+  if (refreshPromise) {
+    return refreshPromise;
+  }
+
+  refreshPromise = refresh();
+
+  try {
+    return await refreshPromise;
+  } finally {
+    refreshPromise = null;
+  }
+};
 
 /**
  * Starts a user session.
@@ -29,7 +119,7 @@ export const signIn = async (
     credentials: "include",
   };
 
-  const response = await sendRequest<SignInResponse>(url, options);
+  const response = await sendRequest<SignInResponse>(url, options, false);
 
   if (response.data?.userSession) {
     setUserSession(response.data.userSession);
@@ -56,7 +146,7 @@ export const signUp = async (
     credentials: "include",
   };
 
-  const response = await sendRequest<SignUpResponse>(url, options);
+  const response = await sendRequest<SignUpResponse>(url, options, false);
 
   if (response.data?.userSession) {
     setUserSession(response.data.userSession);
@@ -68,8 +158,7 @@ export const signUp = async (
 /**
  * Ends a user session.
  *
- * The user session is ended without waiting for a response from the sign out
- * API call.
+ * The user session is ended without waiting for a response from the API.
  */
 export const signOut = (): void => {
   const { clearUserSession, userSession } = useAppStore.getState();
@@ -86,5 +175,29 @@ export const signOut = (): void => {
   };
 
   clearUserSession();
-  sendRequest<void>(url, options);
+  sendRequest<void>(url, options, false);
+};
+
+/**
+ * Refreshes a user session.
+ * @param request The request object.
+ * @returns An ApiResponse object.
+ */
+export const refresh = async (): Promise<ApiResponse<RefreshResponse>> => {
+  const { setUserSession } = useAppStore.getState();
+
+  const url = `${baseUrl}/auth/refresh`;
+  const options: RequestInit = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    credentials: "include",
+  };
+
+  const response = await sendRequest<RefreshResponse>(url, options, false);
+
+  if (response.data?.userSession) {
+    setUserSession(response.data.userSession);
+  }
+
+  return response;
 };
