74b4cb67f23d3fbf4d259344b8b3a0c9915e02b3
Coding Shenanigans <94819102+coding-shenanigans@users.noreply.github.com>
2026-07-21 23:52:36 -0700

    Add navigation to the wish list page (#27)

--- src/components/profile/WishListTable.tsx
+++ src/components/profile/WishListTable.tsx
@@ -10,38 +10,20 @@ import {
 } from "@mui/material";
 import type { WishList } from "../../types";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
-import PublicIcon from "@mui/icons-material/Public";
-import PeopleIcon from "@mui/icons-material/People";
-import LockIcon from "@mui/icons-material/Lock";
-import QuestionMarkIcon from "@mui/icons-material/QuestionMark";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import {
   useState,
   type Dispatch,
-  type JSX,
   type MouseEvent,
   type SetStateAction,
 } from "react";
-
-/**
- * Maps a visibility string to the corresponding icon.
- * @param visibility The visibility string.
- * @returns An icon element.
- */
-const getVisibilityIcon = (visibility: string): JSX.Element => {
-  if (visibility === "public") {
-    return <PublicIcon />;
-  } else if (visibility === "friends_only") {
-    return <PeopleIcon />;
-  } else if (visibility === "private") {
-    return <LockIcon />;
-  } else {
-    return <QuestionMarkIcon />;
-  }
-};
+import { Link as RouterLink } from "react-router";
+import VisibilityIcon from "../shared/VisibilityIcon";
 
 interface WishListTableProps {
+  // TODO: Find the best way to handle a potentially undefined string value.
+  username?: string;
   isProfileOwner: boolean;
   wishLists: WishList[];
   setSelectedWishList: Dispatch<SetStateAction<WishList | null>>;
@@ -112,9 +94,12 @@ export default function WishListTable(props: WishListTableProps) {
             ) : undefined
           }
         >
-          <ListItemButton>
+          <ListItemButton
+            component={RouterLink}
+            to={`/users/${props.username}/wish-lists/${wishList.id}`}
+          >
             <ListItemIcon>
-              {getVisibilityIcon(wishList.visibility)}
+              <VisibilityIcon visibility={wishList.visibility} />
             </ListItemIcon>
             <ListItemText primary={wishList.name} />
           </ListItemButton>
